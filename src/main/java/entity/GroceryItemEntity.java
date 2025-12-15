a4603dc3d0fd732619ca1149ed3b52478596b3c9
malabed1200 <malabed@student.tgm.ac.at>
2025-12-15 14:59:34 +0100

    add GroceryItemEntity

---
+++ src/main/java/entity/GroceryItemEntity.java
@@ -0,0 +1,56 @@
+package entity;
+
+import jakarta.persistence.*;
+
+@Entity
+@Table(name = "GROCERY_ITEM")
+public class GroceryItemEntity {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    private Long id;
+
+    @Column(nullable = false)
+    private String name;
+
+    @Column(nullable = false)
+    private int amount;
+
+    @Column(nullable = false)
+    private boolean collected;
+
+    public GroceryItemEntity() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public boolean isCollected() {
+        return collected;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public void setCollected(boolean collected) {
+        this.collected = collected;
+    }
+}
