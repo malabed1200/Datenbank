ee56bd230db0c288de684989d7d05afe5e8836e4
malabed1200 <malabed@student.tgm.ac.at>
2025-12-15 14:56:22 +0100

    add package structure

---
+++ src/main/java/at/ac/tgm/datenbank/DatenbankApplication.java
@@ -0,0 +1,13 @@
+package at.ac.tgm.datenbank;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DatenbankApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(DatenbankApplication.class, args);
+    }
+
+}
---
+++ src/test/java/at/ac/tgm/datenbank/DatenbankApplicationTests.java
@@ -0,0 +1,13 @@
+package at.ac.tgm.datenbank;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DatenbankApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
