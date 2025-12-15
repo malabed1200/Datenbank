a2e2328fcb4ab5e7c95ba5994402d834be5d6c96
malabed1200 <malabed@student.tgm.ac.at>
2025-12-15 15:36:25 +0100

    connect h2 database inspector

--- src/main/java/at/ac/tgm/datenbank/DatenbankApplication.java
+++ src/main/java/at/ac/tgm/datenbank/DatenbankApplication.java
@@ -1,13 +1,34 @@
 package at.ac.tgm.datenbank;
 
+import at.ac.tgm.datenbank.dto.GroceryItemDto;
+import at.ac.tgm.datenbank.service.GroceryListService;
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DatenbankApplication {
+public class DatenbankApplication implements CommandLineRunner {
+
+    private final GroceryListService service;
+
+    public DatenbankApplication(GroceryListService service) {
+        this.service = service;
+    }
 
     public static void main(String[] args) {
         SpringApplication.run(DatenbankApplication.class, args);
     }
 
+    @Override
+    public void run(String... args) {
+        System.out.println("=== Grocery Items aus der Datenbank ===");
+
+        for (GroceryItemDto item : service.findAll()) {
+            System.out.println(
+                    item.getName() +
+                            " | Menge: " + item.getAmount() +
+                            " | Gesammelt: " + item.isCollected()
+            );
+        }
+    }
 }
