577cbed23bd953c07ed8f7a1719e2a5b4d660316
malabed1200 <malabed@student.tgm.ac.at>
2025-12-15 15:01:05 +0100

    add GroceryItemDto

---
+++ src/main/java/dto/GroceryItemDto.java
@@ -0,0 +1,44 @@
+package dto;
+
+public class GroceryItemDto {
+
+    private Long id;
+    private String name;
+    private int amount;
+    private boolean collected;
+
+    public GroceryItemDto() {
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public int getAmount() {
+        return amount;
+    }
+
+    public boolean isCollected() {
+        return collected;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setAmount(int amount) {
+        this.amount = amount;
+    }
+
+    public void setCollected(boolean collected) {
+        this.collected = collected;
+    }
+}
